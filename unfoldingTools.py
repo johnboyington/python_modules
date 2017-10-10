1ff20dc941435f24d99951df1ec95edc43c606f9
John Boyington <mjcb@ksu.edu>
2017-10-10 17:57:14 -0500

    changed lowercase e in scientific notation to uppercase

--- unfoldingTools.py
+++ unfoldingTools.py
@@ -28,33 +28,35 @@ class BonnerSphereTools(object):
         self.fmtName = None
         self.fluName = None
         self.outName = None
-        self.finalChiSqr = None
-        self.temp =  [1.0, 0.85]
+        self.inpName = None
+        self.finalChiSqr = 1.1
+        self.temp = [1.0, 0.85]
         self.solnStructure = 2
         self.solnRepresentation = 1
-        self.scaling = None
+        self.scaling = [0, 1, 1]
     
     
     def writeMeasuredData(self):
         
-        ibuString  = 'Input file for Bonner Spheres\n'
+        ibuString  = 'Measured Responses for Bonner Spheres\n'
         ibuString += '   {}   {}\n'.format(len(self.sphereSizes), self.correctionFactor)
         for i, names in enumerate(self.sphereIDs):
-            ibuString += '{}   {}   {}   {}   {}   {}   {}\n'.format(self.sphereIDs[i][0], self.sphereSizes[i], self.responseData[i], self.responseData[i] * self.responseError[i], self.responseError[i], self.extraError[i], i)
+            ibuString += '{}   {:4.1f}   {:4.3E}   {:4.3E}   {:5.4f}   {:5.4f}   {:2d}\n'.format(self.sphereIDs[i][0], self.sphereSizes[i], self.responseData[i], self.responseData[i] * self.responseError[i], self.responseError[i], self.extraError[i], i + 1)
         ibuString += '\n'
-        
-        return ibuString
+        with open('{}.ibu'.format(self.ibuName), 'w+') as F:
+            F.write(ibuString)
+        return
     
     def writeResponseFunctions(self):
         if self.rfErgUnits == 'eV' : self.rfIEU = 0
         if self.rfErgUnits == 'MeV' : self.rfIEU = 1
         if self.rfErgUnits == 'keV' : self.rfIEU = 2
         fmtString  = 'Response Function file for Bonner Spheres\n'
-        fmtString += 'Contains {} spheres and {} energy groups using units of {}\n'.format(len(self.sphereIDs), len(self.rfErgEdges), self.ergUnits)
+        fmtString += 'Contains {} spheres and {} energy groups using units of {}\n'.format(len(self.sphereIDs), len(self.rfErgEdges), self.rfErgUnits)
         fmtString += '  {}  {}\n'.format(len(self.rfErgEdges), self.rfIEU)
         
         for i, erg in enumerate(self.rfErgEdges):
-            fmtString += '{:4.3e} '.format(erg)
+            fmtString += '{:4.3E} '.format(erg)
             if (i + 1) % 8 == 0:
                 fmtString += '\n'
                 eolFlag = 1
@@ -70,19 +72,18 @@ class BonnerSphereTools(object):
             fmtString += '{}  {}\n'.format(names[0], names[1])
             fmtString += '1.000E+00      cm^2         0         0    3    1    1    0\n'
             
-            LHS = i * len(self.rfErgEdges)
-            RHS = (i + 1) * len(self.rfErgEdges) 
-            for i, resp in enumerate(self.responses[LHS:RHS]):
-                fmtString += '{:4.3e} '.format(resp)
-                if (i + 1) % 8 == 0:
+            for j, resp in enumerate(self.responses[i]):
+                fmtString += '{:4.3E} '.format(resp)
+                if (j + 1) % 8 == 0:
                     fmtString += '\n'
                     eolFlag = 1
                 else:
                     eolFlag = 0
-                if eolFlag == 0:
-                    fmtString += '\n'
-        
-        return fmtString
+            if eolFlag == 0:
+                fmtString += '\n'
+        with open('{}.fmt'.format(self.fmtName), 'w+') as F:
+            F.write(fmtString)
+        return
     
     def writeDefaultSpectrum(self):
         if self.dsErgUnits == 'eV' : self.dsIEU = 0
@@ -90,23 +91,27 @@ class BonnerSphereTools(object):
         if self.dsErgUnits == 'keV' : self.dsIEU = 2
         fluString  = 'Default Spectrum for Bonner Sphere Unfolding\n'
         fluString += '   {}   {}\n'.format(self.mode, self.dsIEU)
-        fluString += '       2         {}        {}       {:4.3e}\n'.format(len(self.dS), len(self.dS), max(self.dsErgEdges))
+        fluString += '       2         {}        {}       {:4.3E}\n'.format(len(self.dS), len(self.dS), max(self.dsErgEdges))
         for i, value in enumerate(self.dS):
-            fluString += '{:4.3e}  {:4.3e}  {:4.3e}\n'.format(self.dsErgEdges[i], value, self.dsErr[i])
-        return fluString
+            fluString += '{:4.3E}  {:4.3E}  {:4.3E}\n'.format(self.dsErgEdges[i], value, self.dsErr[i])
+        with open('{}.flu'.format(self.fluName), 'w+') as F:
+            F.write(fluString)
+        return
 
     
     def writeControlFile(self):
-        inpString  = '{}   \n'.format(self.ibuName)
-        inpString += '{}   \n'.format(self.fmtName)
+        inpString  = '{}.ibu   \n'.format(self.ibuName)
+        inpString += '{}.fmt   \n'.format(self.fmtName)
         inpString += '{}   \n'.format(self.outName)
-        inpString += '{}   \n'.format(self.fluName)
+        inpString += '{}.flu   \n'.format(self.fluName)
         inpString += '{}   \n'.format(max(self.rfErgEdges))
         inpString += '{}   \n'.format(self.finalChiSqr)
         inpString += '{}, {}   \n'.format(self.temp[0], self.temp[1])
         inpString += '{}, {}   \n'.format(self.solnStructure, self.solnRepresentation)
         inpString += '{}   \n{}   \n{}\n'.format(self.scaling[0], self.scaling[1], self.scaling[2])
-        return inpString
+        with open('{}.inp'.format(self.inpName), 'w+') as F:
+            F.write(inpString)
+        return
     
     def writeMaxedFiles(self):
         self.writeMeasuredData()
